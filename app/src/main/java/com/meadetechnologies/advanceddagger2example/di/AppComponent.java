0b8f393ab391e3756d78ffb9c3c9800b5a47bb49
Nathan <nathan.r.meade@gmail.com>
2020-05-21 16:16:37 -0400

    feat: Application component implemented

---
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/di/AppComponent.java
@@ -0,0 +1,23 @@
+package com.meadetechnologies.advanceddagger2example.di;
+
+import android.app.Application;
+
+import com.meadetechnologies.advanceddagger2example.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(modules = {AndroidSupportInjectionModule.class, })
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
