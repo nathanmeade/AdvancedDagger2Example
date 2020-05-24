41bf69eb37ba6ddca705834036682eff19fcd28b
Nathan <nathan.r.meade@gmail.com>
2020-05-24 11:51:24 -0400

    feat: Singleton scope implemented

--- app/src/main/java/com/meadetechnologies/advanceddagger2example/di/AppComponent.java
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/di/AppComponent.java
@@ -5,11 +5,14 @@ import android.app.Application;
 import com.meadetechnologies.advanceddagger2example.AuthActivity;
 import com.meadetechnologies.advanceddagger2example.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(modules = {AndroidSupportInjectionModule.class, ActivityBuildersModule.class, AppModule.class,})
 public interface AppComponent extends AndroidInjector<BaseApplication> {
 
--- app/src/main/java/com/meadetechnologies/advanceddagger2example/di/AppModule.java
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/di/AppModule.java
@@ -10,22 +10,27 @@ import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.meadetechnologies.advanceddagger2example.R;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions(){
         return RequestOptions.placeholderOf(R.drawable.white_background).error(R.drawable.white_background);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions){
         return Glide.with(application).applyDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application){
         return ContextCompat.getDrawable(application, R.drawable.logo);
