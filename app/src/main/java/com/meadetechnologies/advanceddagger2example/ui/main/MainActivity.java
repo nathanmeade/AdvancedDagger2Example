9821b0e243fd1812199aa05427f29ad8c88c5f22
Nathan <nathan.r.meade@gmail.com>
2020-05-25 11:20:23 -0400

    feat: Toolbar menu with logout option implemented

--- app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/main/MainActivity.java
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/main/MainActivity.java
@@ -1,12 +1,16 @@
 package com.meadetechnologies.advanceddagger2example.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.meadetechnologies.advanceddagger2example.BaseActivity;
 import com.meadetechnologies.advanceddagger2example.R;
+import com.meadetechnologies.advanceddagger2example.SessionManager;
 
 public class MainActivity extends BaseActivity {
 
@@ -18,5 +22,23 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "mainactivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
