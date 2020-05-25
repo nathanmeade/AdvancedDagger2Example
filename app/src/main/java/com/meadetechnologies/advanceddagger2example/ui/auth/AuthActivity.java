bb034e797eaef00171c4746de3471cf1d757d7b0
Nathan <nathan.r.meade@gmail.com>
2020-05-24 21:03:44 -0400

    feat: Authentication network resource implemented

--- app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/auth/AuthActivity.java
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/auth/AuthActivity.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.RequestManager;
@@ -42,6 +44,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     Drawable appDrawable;
 
     private EditText editText;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_auth);
 
         editText = findViewById(R.id.user_id_input);
+        progressBar = findViewById(R.id.progress_bar);
 
         findViewById(R.id.login_button).setOnClickListener(this);
 
@@ -60,14 +64,40 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     }
 
     private void subscribeObservers(){
-        authViewModel.observeUser().observe(this, new Observer<User>() {
+        authViewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
-                if (user != null){
-                    Log.d(TAG, "onChanged: user is authenticated" + user.getEmail());
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case LOADING:{
+                            Log.d(TAG, "onChanged: LOADING ");
+                            showProgressBar(true);
+                        break;}
+                        case AUTHENTICATED:{
+                            showProgressBar(false);
+                            Log.d(TAG, "onChanged: LOGIN SUCCESS: " + userAuthResource.data.getEmail());
+                        break;}
+                        case ERROR:{
+                            Log.d(TAG, "onChanged: " + userAuthResource.message);
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, userAuthResource.message + "\nDid you enter a number between 1 and 10?", Toast.LENGTH_SHORT).show();
+                        break;}
+                        case NOT_AUTHENTICATED:{
+                            showProgressBar(false);
+                        break;}
+                    }
                 }
             }
         });
+    }
+
+    private void showProgressBar(Boolean isVisible){
+        if (isVisible){
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        else {
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
     private void setLogo(){
