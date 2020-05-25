62dc56e61742ccd8403f87d12ea8057ba25f680a
Nathan <nathan.r.meade@gmail.com>
2020-05-25 14:58:03 -0400

    feat: Profile ViewModel data instantiation implemented

--- app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/meadetechnologies/advanceddagger2example/ui/main/profile/ProfileFragment.java
@@ -5,13 +5,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.meadetechnologies.advanceddagger2example.R;
+import com.meadetechnologies.advanceddagger2example.models.User;
+import com.meadetechnologies.advanceddagger2example.ui.auth.AuthResource;
 import com.meadetechnologies.advanceddagger2example.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -23,6 +27,7 @@ public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
 
     private ProfileViewModel viewModel;
+    private TextView email, username, website;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -31,6 +36,7 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
 
     }
@@ -38,6 +44,48 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: Profile fragment was created");
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+        subscribeObservers();
     }
+
+    private void subscribeObservers(){
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case AUTHENTICATED: {
+                            Log.d(TAG, "onChanged: authenticated on profile fragments");
+                            setUserDetails(userAuthResource.data);
+                            break;
+                        }
+                        case ERROR: {
+                            Log.d(TAG, "onChanged: authentication error on profile fragments");
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        username.setText("error");
+        website.setText("error");
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
+    }
+
+
 }
